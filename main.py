4ac9a17a18928db1fdfb4a91fbce5d2f28c8fa7f
Juraj Herda <juraj.herda1983@gmail.com>
2023-04-26 21:01:38 +0200

    Testing changing size of pie chart

--- main.py
+++ main.py
@@ -686,6 +686,7 @@ my_colors = ["#218727", "#d00"]
 my_explode = [0.1, 0]
 
 fig = Figure()
+fig.set_size_inches(6, 4)
 ax = fig.add_subplot(111)
 ax.pie(year_annual_turnover, labels=my_labels, colors=my_colors, explode=my_explode, shadow=True)
 ax.legend(title="Celkový ročný obrat", loc='center left', bbox_to_anchor=(0.7, 1.02))
